59401b3e3beb64edc8f515a01250f74f9d45ccea
zx <346084070@qq.com>
2022-04-01 21:12:28 +0800

    remove something

--- init/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import ElementPlus from 'element-plus'
-import VueRouter from "vue-router"
-import 'element-plus/dist/index.css'
-
-createApp(App).use(VueRouter).use(ElementPlus).mount('#app')
